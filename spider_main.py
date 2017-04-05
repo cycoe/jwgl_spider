d023986a850e151b77cb2dacb0325ccb18ddcef1
cycoe <cycoe@cycoe.win>
2017-04-05 15:27:09 +0800

    fetch grade

--- spider_main.py
+++ spider_main.py
@@ -1,82 +1,148 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+from requests import Session, Request, exceptions
+
 
 class Spider(object):
     def __init__(self):
-        self.checkUrl = "http://jwgl.buct.edu.cn/CheckCode.aspx"
-        self.postUrl = "http://jwgl.buct.edu.cn/default2.aspx"
-        self.username = '2013012433'
-        self.password = 'zhn1106'
+        self.verifyCodeUrl = "http://jwgl.buct.edu.cn/CheckCode.aspx"
+        #self.jwglLoginUrl = "http://jwgl.buct.edu.cn"
+        self.jwglLoginUrl = "http://jwgl.buct.edu.cn/default2.aspx"
+        self.getGradeUrl = "http://jwgl.buct.edu.cn/xscjcx.aspx"
+        self.vpnLoginUrl = "https://vpn.buct.edu.cn/dana-na/auth/url_default/login.cgi"
+        self.vpnUrl = "https://vpn.buct.edu.cn/dana-na/auth/url_default/welcome.cgi"
+        self.vpnHomeUrl = "https://vpn.buct.edu.cn/dana/home/index.cgi"
+        self.studentID = '2013012433'
+        self.username = '朱浩南'
+        self.jwglPassword = 'zhn1106'
+        self.vpnPassword = 'zhiwen.COM'
 
-    def formatPostData(self):
+        self.session = requests.Session()
+        self.response = self.session.send(self.prepareJwglFirst(), timeout=5)
+        print(self.response.url)
+
+        # 实例化验证码识别器对象，识别验证码
+        from classifier import Classifier
+        self.classifier = Classifier()
+        self.classifier.loadTrainingMat()
+
+    def formatHeaders(self, referer=None):
+        headers = {
+            'Host': 'jwgl.buct.edu.cn',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Request': '1',
+            }
+        if referer:
+            headers['Referer'] = referer
+        return headers
+
+    def getVIEWSTATE(self):
+        import re
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(self.response.text, "html.parser")
+        print(re.findall('<.*name="__VIEWSTATE".*value="(.*)?".*/>', str(soup.input))[0])
+        return re.findall('<.*name="__VIEWSTATE".*value="(.*)?".*/>', str(soup.input))[0]
+
+    def prepareJwglFirst(self):
+        headers = self.formatHeaders()
+        req = Request('GET', self.jwglLoginUrl, headers=headers)
+        return self.session.prepare_request(req)
+
+    def prepareJwglLogin(self):
         """
-        格式化 post 和 headers 键值对
+        准备登录 jwgl 需要的 request
         """
-        import urllib
-        import urllib.request
         postdata = {
-            '__VIEWSTATE': 'dDwyODE2NTM0OTg7Oz7Au3GhNlfrriMFSCY1HWTKgq5dKA==',
-            'txtUserName': self.username,
-            'TextBox2': self.password,
+            '__VIEWSTATE': self.getVIEWSTATE(),
+            'txtUserName': self.studentID,
+            'TextBox2': self.jwglPassword,
             'txtSecretCode': self.verCode,
             'RadioButtonList1': '学生',
             'Button1': '',
             'lbLanguage': '',
             'hidPdrs': '',
             'hidsc': '',
-            }
+        }
+        headers = self.formatHeaders(self.jwglLoginUrl)
+        req = Request('POST', self.jwglLoginUrl, headers=headers, data=postdata, cookies=self.session.cookies)
+        return self.session.prepare_request(req)
 
-        headers = {
-            'Host': 'jwgl.buct.edu.cn',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Accept-Encoding': 'gzip, deflate',
-            'Referer': 'http://jwgl.buct.edu.cn/(dxzmap55hhkiw4jte53vdi45)/default2.aspx',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Request': '1',
-            }
-        postdata = urllib.parse.urlencode(postdata).encode('utf-8')
-        request = urllib.request.Request(self.postUrl, postdata, headers)
-        return request
+    def prepareGetGrade(self):
+        headers = self.formatHeaders(self.response.url)
+        params = {
+            'xh': self.studentID,
+            'xm': self.username,
+            'gnmkdm': 'N121605',
+        }
+        req = Request('GET', self.getGradeUrl, headers=headers, params=params, cookies=self.session.cookies)
+        return self.session.prepare_request(req)
 
-    def login(self):
+    def preparePastGrade(self):
+        headers = self.formatHeaders(self.response.url)
+        params = {
+            'xh': self.studentID,
+            'xm': self.username,
+            'gnmkdm': 'N121605',
+        }
+        postdata = {
+            '__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__VIEWSTATE': self.getVIEWSTATE(),
+            #'__VIEWSTATEGENERATOR':
+            'hidLanguage': '',
+            'ddlXN': '',
+            'ddlXQ': '',
+            'ddl_kcxz': '',
+            'btn_zcj': '历年成绩',
+        }
+        req = Request('POST', self.getGradeUrl, headers=headers, params=params, data=postdata, cookies=self.session.cookies)
+        return self.session.prepare_request(req)
+
+
+    def jwglLogin(self, tryNum=10):
         """
-        登录教务网，使用 urllib 的 cookie 登录功能
+        登录教务网
         """
-        import urllib.request
-        import http.cookiejar
         import re
-        from classifier import Classifier
-
-        # 创建带 cookie 功能的 opener
-        cookie = http.cookiejar.LWPCookieJar()
-        handler = urllib.request.HTTPCookieProcessor(cookie)
-        opener = urllib.request.build_opener(handler, urllib.request.HTTPHandler)
-        urllib.request.install_opener(opener)
-
-        # 抓取验证码，存储 cookie 对象
-        picture = opener.open(self.checkUrl).read()
-        with open('check.gif','wb') as f:
-            f.write(picture)
-        # 实例化验证码识别器对象，识别验证码
-        classifier = Classifier()
-        classifier.loadTrainingMat()
-        self.verCode = classifier.recognizer("check.gif")
-        try:
-            response = opener.open(self.formatPostData())
-            responseUrl = response.geturl()
-            if not re.search(self.username, responseUrl):
-                raise VerifyError("Wrong Verification code!")
-            else:
-                result = response.read().decode('gb2312')
-                print("login successfully!")
-        except urllib.request.HTTPError as e:
-            print(e.code)
+        tryNum -= 1
+        if tryNum < 0:
+            print('\n*** stack overflow! exiting...')
             exit(0)
+
+        codeImg = self.session.get(self.verifyCodeUrl, timeout=5)
+        with open('check.gif', 'wb') as fr:
+            for chunk in codeImg:
+                fr.write(chunk)
+        self.verCode = self.classifier.recognizer("check.gif")
+
+        try:
+            self.response = self.session.send(self.prepareJwglLogin(), timeout=5)
+            if re.search(self.studentID, self.response.url):
+                print("login successfully!")
+                print(self.response.url)
+            else:
+                print(self.response.request.url)
+                print(self.response.url)
+                #print(self.response.text)
+                raise VerifyError("Wrong Verification code!")
         except VerifyError as e:
             print(e)
             print("retry...")
-            self.login()
+            self.jwglLogin(tryNum)
+
+    def getGrade(self):
+        self.response = self.session.send(self.prepareGetGrade(), timeout=5)
+        self.response = self.session.send(self.preparePastGrade(), timeout=5, allow_redirects=False)
+        print(self.response.text)
+
+    def clean(self):
+        self.session.close()
+
 
 class VerifyError(Exception):
     def __init__(self, errorInfo):
@@ -87,7 +153,9 @@ class VerifyError(Exception):
 
 def main():
     spider = Spider()
-    spider.login()
+    spider.jwglLogin(tryNum=10)
+    spider.getGrade()
+    spider.clean()
 
 if __name__ == "__main__":
     main()
